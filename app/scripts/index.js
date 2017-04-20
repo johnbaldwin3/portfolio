bd6baecc01dfb4014ea502952d66f37ff21da597
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-04-20 09:19:22 -0400

    starting real portfolio

---
+++ app/scripts/index.js
@@ -0,0 +1,31 @@
+var $ = window.jQuery = require('jquery');
+var Materialize = require('materialize-css');
+require('../../node_modules/materialize-css/js/sideNav.js');
+require('../../node_modules/materialize-css/js/parallax.js');
+require('../../node_modules/materialize-css/js/carousel.js');
+
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    // Next slide
+    $('.carousel').carousel('next');
+    $('.carousel').carousel('next', 3); // Move next n times.
+    // Previous slide
+    $('.carousel').carousel('prev');
+    $('.carousel').carousel('prev', 4); // Move prev n times.
+    // Set to nth slide
+    $('.carousel').carousel('set', 4);
+
+    $(document).on('click', '.nav-wrapper a', function(e){
+      e.preventDefault();
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 1000);
+    });
+
+  }); // end of document ready
+})(jQuery);
